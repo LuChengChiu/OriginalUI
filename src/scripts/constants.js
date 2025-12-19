99c138e67fb36d06d4f16928e3fe84c2f66cb545
LuChengChiu <luchengchiu@qiuluchengdeMacBook-Air.local>
2025-12-19 08:26:28 +0800

    chore: remove unused global variable

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -7,6 +7,3 @@ export const MAX_Z_INDEX = 2147483647;
 
 // High z-index threshold for suspicious element detection
 export const HIGH_Z_INDEX_THRESHOLD = 1000000;
-
-// Very high z-index threshold for click hijacking detection  
-export const VERY_HIGH_Z_INDEX_THRESHOLD = 2000000;
